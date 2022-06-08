13dbac0e436dbf441dc54cdb00a99a868d8cbeea
SRIVARI <69836058+Srivari07@users.noreply.github.com>
2022-06-08 19:39:56 +0530

    files added

---
+++ Configs/blocklist.py
@@ -0,0 +1,2 @@
+BlockWords = ["DIP", "", "$", "RH", "YOLO", "PORN", "BEST", "MOON", "HOLD", "FAKE", "WISH", "USD", "EV", "MARK", "RELAX", "LOL", "LMAO",
+              "LMFAO", "EPS", "DCF", "NYSE", "FTSE", "APE", "CEO", "CTO", "FUD", "DD", "AM", "PM", "FDD", "EDIT", "TA", "UK", "AMC", "GME"]
